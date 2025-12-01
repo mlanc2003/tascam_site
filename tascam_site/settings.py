a6968add96cb65f98345371e30d966a2d6dbe465
Massimo Lancia <massimolancia@outlook.com>
2025-12-01 14:30:44 -0500

    Updated site

--- tascam_site/settings.py
+++ tascam_site/settings.py
@@ -66,8 +66,12 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
